d7ed4383ac8bca9ffd77e4ffa0dbc0095f828149
Lee JaeHyun <myleje@gmail.com>
2022-12-04 13:48:55 +0900

    .

--- Assets/02.Scripts/Barrel.cs
+++ Assets/02.Scripts/Barrel.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
+    [SerializeField] private new MeshRenderer renderer;
     public Texture[] textures;
 
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        renderer = GetComponentInChildren<MeshRenderer>();
     }
 }
