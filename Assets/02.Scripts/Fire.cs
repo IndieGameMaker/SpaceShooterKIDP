71504c8e933a181f4167f5586552aea8044a157f
Lee JaeHyun <myleje@gmail.com>
2022-12-04 10:26:30 +0900

    .

---
+++ Assets/02.Scripts/Fire.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // Bullet 프리팹을 저장할 변수
+    [SerializeField] private GameObject bulletPrefab;
+    // 총알을 생성할 위치정보를 저장할 변수
+    [SerializeField] private Transform firePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
