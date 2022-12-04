6dac4bc0266136ad2b199b2d7d9e790b01f83c05
Lee JaeHyun <myleje@gmail.com>
2022-12-04 14:08:59 +0900

    .

--- Assets/02.Scripts/Barrel.cs
+++ Assets/02.Scripts/Barrel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Barrel : MonoBehaviour
 {
@@ -11,5 +12,16 @@ public class Barrel : MonoBehaviour
     void Start()
     {
         renderer = GetComponentInChildren<MeshRenderer>();
+
+        int index = Random.Range(0, textures.Length); // 0, 1, 2
+        renderer.material.mainTexture = textures[index];
     }
 }
+
+/*
+    Random.Range(min, max)
+
+    Random.Range(0, 10) -> 0, 1, 2, ..., 9
+    Random.Range(0.0f, 10.0f) -> 0.0f ~ 10.0f
+
+*/
